b50b3267deede2a18cb2548e7c593e896b788383
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-26 09:21:09 +0300

    Modified files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./styles/globals.css";
+import { ContextProvider } from "../context/index.js";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ContextProvider>{children}</ContextProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <h1>asd</h1>;
+"use client";
+
+import React, { useContext } from "react";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
+import "./styles/auth.css";
+
+export default function Auth() {
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+          <div className="auth-title">Next.js Chat</div>
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+              value={secret}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
