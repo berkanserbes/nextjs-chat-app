e097ab374ed861d993ea0c9fc36ca6350fc782dd
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-26 09:19:18 +0300

    Added context

---
+++ src/context/index.js
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useState, createContext } from "react";
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
+
+  const value = {
+    username,
+    setUsername,
+    secret,
+    setSecret,
+  };
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
